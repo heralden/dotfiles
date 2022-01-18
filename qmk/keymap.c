70fddcce52c165fdd93d7b64903eb826a7e9a54e
uosl <regen@anche.no>
2022-01-18 16:19:36 +0000

    Simplify keymap FN layer and use LAG instead of HYPER
    Never used the "WASD" arrow keys on FN layer.

--- qmk/keymap.c
+++ qmk/keymap.c
@@ -37,7 +37,7 @@ enum layers {
 #define OSM_SFT     OSM(MOD_LSFT)
 #define OSM_RCTL    OSM(MOD_RCTL)
 #define OSM_LALT    OSM(MOD_LALT)
-#define OSM_HYPR    OSM(MOD_HYPR)
+#define OSM_LAG     OSM(MOD_LGUI | MOD_LALT)
 #define OSM_GUI     OSM(MOD_LGUI)
 
 #define CTL_ESC     MT(MOD_LCTL, KC_ESC)
@@ -66,15 +66,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |  \  |  |   Z  |   X  |   C  |   V  |   B  | Esc  | Tab  |  |CapsLk| Enter|   N  |   M  | ,  < | . >  | '  " |  /  ?  |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      | OSM  | OSM  | Back | OSL  |  | OSL  | Space| OSM  | OSM | Pause|
- *                        |      | HYPR | LGUI | Space| FN   |  |NUMSYM|      | RCtl | LAlt|      |
+ *                        | APP  | OSM  | OSM  | Back | OSL  |  | OSL  | Space| OSM  | OSM | Pause|
+ *                        |      | LAG  | GUI  | Space| FN   |  |NUMSYM|      | RCtl | LAlt|      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
      KC_GRV  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y   ,  KC_U ,  KC_I ,   KC_O ,  KC_P , KC_MINS,
      OSM_SFT , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H   ,  KC_J ,  KC_K ,   KC_L ,KC_SCLN, OSM_SFT,
      KC_BSLS , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , CTL_ESC, KC_TAB,     KC_CAPS, ALT_ENT, KC_N   ,  KC_M ,KC_COMM, KC_DOT ,KC_QUOT, KC_SLSH,
-                                 KC_NO ,OSM_HYPR, OSM_GUI, KC_BSPC, FN    ,     NUMSYM , KC_SPC ,OSM_RCTL,OSM_LALT,KC_PAUS
+                                 KC_APP,OSM_LAG , OSM_GUI, KC_BSPC, FN    ,     NUMSYM , KC_SPC ,OSM_RCTL,OSM_LALT,KC_PAUS
     ),
 
 /*
@@ -102,20 +102,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Layer: FN
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        | Vol+ | Home |  Up  | PgUp |      |                              | Play |   {  |   }  |   [  |   ]  |   Å    |
+ * |        |      | Mute | Prev | Next | Play |                              | Play |   {  |   }  |   [  |   ]  |   Å    |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        | Vol- | Left | Down |Right |CtlSftTab|                           | Left | Down |  Up  | Right|   Ø  |   Æ    |
+ * |        |      | Vol+ | Home | PgUp |PrvTab|                              | Left | Down |  Up  | Right|   Ø  |   Æ    |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        | Mute | End  | Play |PgDown|CtlTab|      |      |  |      |      | Home | End  |   (  |   )  |   =  |   +    |
+ * |        |      | Vol- | End  | PgDn |NxtTab|      |      |  |      |      | Home | End  |   (  |   )  |   =  |   +    |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
 	  [_FN] = LAYOUT(
-     KC_NO   ,KC_VOLU, KC_HOME, KC_UP  , KC_PGUP,  KC_NO ,                                          KC_MPLY, KC_LCBR,KC_RCBR, KC_LBRC,KC_RBRC, NOR_AA,
-     KC_NO   ,KC_VOLD, KC_LEFT, KC_DOWN, KC_RGHT, PREV_TAB,                                         KC_LEFT, KC_DOWN, KC_UP , KC_RGHT,NOR_OE , NOR_AE,
-     KC_NO   ,KC_MUTE, KC_END , KC_MPLY, KC_PGDN, NEXT_TAB, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_HOME, KC_END ,KC_LPRN, KC_RPRN,KC_EQL , KC_PLUS,
+     KC_NO   , KC_NO , KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY ,                                          KC_MPLY, KC_LCBR,KC_RCBR, KC_LBRC,KC_RBRC, NOR_AA,
+     KC_NO   , KC_NO , KC_VOLU, KC_HOME, KC_PGUP, PREV_TAB,                                         KC_LEFT, KC_DOWN, KC_UP , KC_RGHT,NOR_OE , NOR_AE,
+     KC_NO   , KC_NO , KC_VOLD, KC_END , KC_PGDN, NEXT_TAB, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_HOME, KC_END ,KC_LPRN, KC_RPRN,KC_EQL , KC_PLUS,
                                 KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 

4f6f21f375c518db09a8caab7615ae8c21b4ecce
heralden <regen@anche.no>
2022-10-11 11:11:42 +0100

    lein profiles.clj JNA apple silicon fix

---
+++ .lein/profiles.clj
@@ -0,0 +1,3 @@
+{:user {;; Use this version of JNA when present as a dependency (likely of figwheel in the dev profile).
+        ;; https://stackoverflow.com/a/70369813
+        :managed-dependencies [[net.java.dev.jna/jna "5.12.1"]]}}

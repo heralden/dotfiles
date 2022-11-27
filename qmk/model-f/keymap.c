d8e492233a269e779eb32446857abbd45d6e7053
heralden <regen@anche.no>
2022-11-27 22:51:28 +0000

    Lisp parens key

--- qmk/model-f/keymap.c
+++ qmk/model-f/keymap.c
@@ -37,14 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT_ansi_hhkb_split_shift_split_backspace(
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSLS,     KC_P7,   KC_P8,   KC_P9,
-        KC_ESC,    KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_TAB,         KC_P4,   KC_P5,   KC_P6,
+        KC_ESC,    KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LPRN, KC_RPRN, KC_TAB,         KC_P4,   KC_P5,   KC_P6,
         FN_BSPC,      KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,            KC_P1,   KC_P2,   KC_P3,
         OSM_LSFT,        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_FN),   KC_P0,   KC_UP,   KC_PDOT,
         KC_LGUI, KC_LCTL, KC_LALT,                      KC_SPC,                  KC_RALT, KC_RCTL, KC_RGUI,      KC_LEFT, KC_DOWN, KC_RIGHT
     ),
     [_FN] = LAYOUT_ansi_hhkb_split_shift_split_backspace(
         _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, KC_CAPS,     KC_HOME,   KC_UP,   KC_PGUP,
-        _______,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                PREV_TAB,  KC_NO,   NEXT_TAB,
+        _______,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_NO,            PREV_TAB,  KC_NO,   NEXT_TAB,
         _______,      KC_NO, KC_NO, KC_NO, KC_NO, PREV_TAB, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_NO, KC_NO, KC_NO,           KC_END,    KC_DOWN, KC_PGDN,
         _______,         KC_NO, KC_NO, KC_NO, KC_NO, NEXT_TAB, KC_NO, KC_NO, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______,     KC_INS,    KC_VOLU, KC_DEL,
         _______, _______, _______,                             MO(_FN2),                       _______, _______, _______,     KC_MUTE,   KC_VOLD, KC_MPLY

e371ea9be6bbd363e439d0167458003b94e2c26d
heralden <regen@anche.no>
2022-12-16 12:18:06 +0000

    Try mods on left column

--- qmk/model-f/keymap.c
+++ qmk/model-f/keymap.c
@@ -30,6 +30,8 @@ enum custom_keycodes {
 
 #define OSM_LSFT OSM(MOD_LSFT)
 #define FN_BSPC LT(_FN, KC_BSPC)
+#define ALT_ESC MT(MOD_LALT, KC_ESC)
+#define CTL_BSPC MT(MOD_LCTL, KC_BSPC)
 #define NEXT_TAB LCTL(KC_TAB)
 #define PREV_TAB LCTL(LSFT(KC_TAB))
 
@@ -37,8 +39,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT_ansi_hhkb_split_shift_split_backspace(
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSLS,     KC_P7,   KC_P8,   KC_P9,
-        KC_ESC,    KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LPRN, KC_RPRN, KC_TAB,         KC_P4,   KC_P5,   KC_P6,
-        FN_BSPC,      KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,            KC_P1,   KC_P2,   KC_P3,
+        ALT_ESC,   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LPRN, KC_RPRN, KC_TAB,         KC_P4,   KC_P5,   KC_P6,
+        CTL_BSPC,     KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,            KC_P1,   KC_P2,   KC_P3,
         OSM_LSFT,        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_FN),   KC_P0,   KC_UP,   KC_PDOT,
         KC_LGUI, KC_LCTL, KC_LALT,                      KC_SPC,                  KC_RALT, KC_RCTL, KC_RGUI,      KC_LEFT, KC_DOWN, KC_RIGHT
     ),
